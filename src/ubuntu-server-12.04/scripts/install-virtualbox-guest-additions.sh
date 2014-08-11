dc95730a8156c3d382059e37cdc5bb9274eeb42a
Radu Helstern <radu.helstern@gmail.com>
2014-08-12 00:30:10 +0100

    fix install virtualbox install script

--- src/ubuntu-server-12.04/scripts/install-virtualbox-guest-additions.sh
+++ src/ubuntu-server-12.04/scripts/install-virtualbox-guest-additions.sh
@@ -3,6 +3,12 @@
 set -e
 set -x
 
+# check that script is running with elevated privileges
+if [ $(/usr/bin/id -u) -ne 0 ]; then
+    echo "Not running as root or using sudo"
+    exit 1
+fi
+
 #install linux headers
 aptitude install -y linux-headers-$(uname -r)
 
@@ -13,15 +19,17 @@ aptitude install -y make
 
 # Mount the disk image
 cd /tmp
+
 MOUNT_DIR=/tmp/mount-virtualbox-guest-additions
-VBOX_ISO_LOCATION=/home/radu
-mkdir -p $MOUNT_DIR
-mount -t iso9660 -o loop "$VBOX_ISO_LOCATION/VBoxGuestAdditions.iso" $MOUNT_DIR
+mkdir -p ${MOUNT_DIR}
+
+VBOX_ISO_LOCATION=/home/manager/VBoxGuestAdditions.iso
+mount -t iso9660 -o loop ${VBOX_ISO_LOCATION} ${MOUNT_DIR}
 
 # Install the drivers
-"$MOUNT_DIR/VBoxLinuxAdditions.run"
+${MOUNT_DIR}/VBoxLinuxAdditions.run --nox11
 
 # Cleanup
-umount "$MOUNT_DIR"
+umount -vf ${MOUNT_DIR}
 #rm -rf isomount /root/VBoxGuestAdditions.iso
-rm -rf "$MOUNT_DIR"
+rm -rf ${MOUNT_DIR}
