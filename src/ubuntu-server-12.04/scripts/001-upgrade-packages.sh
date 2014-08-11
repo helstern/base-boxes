17745b90020588730d04e4f7dac98b730c4797bb
Radu Helstern <radu.helstern@gmail.com>
2014-08-12 00:41:12 +0100

    add bash script to upgrade package

---
+++ src/ubuntu-server-12.04/scripts/001-upgrade-packages.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+set -x
+
+# check that script is running with elevated privileges
+if [ $(/usr/bin/id -u) -ne 0 ]; then
+    echo "Not running as root or using sudo"
+    exit 1
+fi
+aptitude --assume-yes update
+aptitude --assume-yes full-upgrade
