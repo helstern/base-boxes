ad4db43e2e5f478780eaf0585ec6c89a1ce4198f
Radu Helstern <radu.helstern@gmail.com>
2014-08-12 01:25:26 +0100

    fix late_command script

--- src/ubuntu-server-12.04/http/late_command.sh
+++ src/ubuntu-server-12.04/http/late_command.sh
@@ -7,7 +7,4 @@ echo 'runnning preseed/late_command'
 cp /etc/udev/rules.d/70-persistent-net.rules /etc/udev/rules.d/70-persistent-net.bak
 rm /etc/udev/rules.d/70-persistent-net.rules
 
-apt-get update -y
-apt-get upgrade -y --force-yes
-
 apt-get install -y --force-yes xauth
