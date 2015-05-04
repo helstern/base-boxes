b2627a174b0eadd7793d087d6fa56930ac36704e
radu helstern <radu.helstern@gmail.com>
2015-05-04 14:40:17 +0100

    add opensuse iso download commands

--- iso/download.sh
+++ iso/download.sh
@@ -19,6 +19,10 @@ SOURCE=http://download.opensuse.org/distribution/13.2/iso/openSUSE-13.2-GNOME-Li
 FILENAME=openSUSE-13.2-GNOME-Live-x86_64.iso
 wget --output-document=${DOWNLOAD_ROOT_DIR}/${FILENAME} ${SOURCE}
 
+SOURCE=http://download.opensuse.org/distribution/13.2/iso/openSUSE-13.2-DVD-x86_64.iso
+FILENAME=openSUSE-13.2-DVD-x86_64.iso
+wget --output-document=${DOWNLOAD_ROOT_DIR}/${FILENAME} ${SOURCE}
+
 SOURCE=http://releases.ubuntu.com/12.04/ubuntu-12.04.5-desktop-amd64.iso
 FILENAME=ubuntu-12.04.5-desktop-amd64.iso
 wget --output-document=${DOWNLOAD_ROOT_DIR}/${FILENAME} ${SOURCE}
