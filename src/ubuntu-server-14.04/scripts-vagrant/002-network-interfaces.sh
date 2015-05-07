96056befed49eb563b3f0942f7ccd66aff8adc31
radu helstern <radu.helstern@gmail.com>
2015-05-08 00:51:47 +0100

    fix qemu build for ubuntu server 14.04

---
+++ src/ubuntu-server-14.04/scripts-vagrant/002-network-interfaces.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+set -x
+
+# check that script is running with elevated privileges
+if [ $(/usr/bin/id -u) -ne 0 ]; then
+    echo "Not running as root or using sudo"
+    exit 1
+fi
+
+mkdir -p /etc/network/interfaces.d
+
+mv /etc/network/interfaces /etc/network/interfaces.bak
+
+# remove generated net rules because importing the appliance will attach network cards with different macs
+# rm -f /etc/udev/rules.d/70-persistent-net.rules
+
+#loopback interface
+cat <<INTERFACES > /etc/network/interfaces
+#the loopback interface
+auto lo
+iface lo inet loopback
+
+source /etc/network/interfaces.d/*.conf
+INTERFACES
+
+#primary interface
+cat <<ETHO > /etc/network/interfaces.d/010-eth0.conf
+auto eth0
+iface eth0 inet dhcp
+ETHO
+
+# second interface
+cat <<ETH1 > /etc/network/interfaces.d/015-eth1.conf
+auto eth1
+iface eth1 inet dhcp
+ETH1
+
+SLEEP_DURATION=15
+echo "sleeping ${SLEEP_DURATION} seconds"
+sleep "${SLEEP_DURATION}"
