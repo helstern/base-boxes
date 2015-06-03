cbd3c42bd7dc90871b7c41c9f3737b47a60f232b
radu helstern <radu.helstern@gmail.com>
2015-06-04 00:16:25 +0100

    improve pipeline scripts

--- bin/qemu2vagrant.build-pipeline.sh
+++ bin/qemu2vagrant.build-pipeline.sh
@@ -3,21 +3,21 @@
 BUILD_NUMBER=latest
 BUILD_NAME_PREFIX=
 BUILD=
+BASE_BUILD=
 BUILD_TYPE=qcow2
 
 function usage() {
     echo 'this script builds a '
-
-    echo 'qemu2vagrant.build-pipeline.sh [--number latest] --prefix ubuntu-server-14.04'
+    echo 'qemu2vagrant.build-pipeline.sh [--base ubuntu-server-14.04] [--build-number latest] -- ubuntu-server-14.04'
 }
 
 function check_dependencies() {
-    if which sort head wc packer qemu-img > /dev/null 2>&1; then
+    if which sort head wc packer qemu-img vagrant > /dev/null 2>&1; then
         echo 'all dependencies met'
         return 0
     fi
 
-    echo 'the following commands must be available: sort head wc'
+    echo 'the following commands must be available: sort head wc packer qemu-img vagrant' 1>&2
     return 1
 }
 
@@ -33,15 +33,20 @@ function parse_arguments() {
     while [ $# -gt 0 ]
     do
         case "${1}" in
-            -b|--build)
-                BUILD="${2}"
+            -b|--base)
+                BASE_BUILD="${2}"
+                if [ -z "${BASE_BUILD}" ]; then
+                    echo 'invalid prefix parameter specified'
+                    exit 2
+                fi
+
                 shift
             ;;
             -h|--help)
                 usage
                 exit 1
             ;;
-            -n|--number)
+            -n|--build-number)
                 BUILD_NUMBER="${2}"
                 shift
             ;;
@@ -61,7 +66,7 @@ function parse_arguments() {
             ;;
             --) # End of all options
                 shift
-                INPUT="${1}"
+                BUILD="${1}"
                 break;
             ;;
             -*)
@@ -75,18 +80,18 @@ function parse_arguments() {
 
 function assert_qemu_build_src() {
     if [ "$(echo "${1}" | wc --lines)" != "1" ]; then
-        echo 'found more than one build dir:'
-        echo "${1}"
+        echo 'found more than one build dir:' 1>&2
+        echo "${1}" 1>&2
         exit 1
     fi
 
     if ! test -f "${1}/manifest.qemu.packer.json"; then
-        echo "missing base qemu manifest ${1}/manifest.qemu.packer.json"
+        echo "missing base qemu manifest ${1}/manifest.qemu.packer.json" 1>&2
         exit 1
     fi
 
     if ! test -f "${1}/vagrant.manifest.qemu.packer.json"; then
-        echo "missing base qemu manifest ${1}/vagrant.manifest.qemu.packer.json"
+        echo "missing base qemu manifest ${1}/vagrant.manifest.qemu.packer.json" 1>&2
         exit 1
     fi
 }
@@ -95,40 +100,38 @@ parse_arguments "$@"
 assert_conflicting_arguments
 
 PROJECT_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )/../" && pwd)
+
+if [ -n "${BASE_BUILD}" ]; then
+    BASE_QEMU_ARTIFACT=$(bash "${PROJECT_DIR}"/bin/find-build-artifact.sh --number latest --prefix "${BASE_BUILD}" --type "${BUILD_TYPE}" 2>/dev/null)
+fi
+
 # build number is the unix timestamp
 BUILD_NUMBER=$(date +%s)
-BASE_QEMU_ARTIFACT="${PROJECT_DIR}/target/${BUILD}-${BUILD_NUMBER}/${BUILD}.qcow2"
-VAGRANT_QEMU_ARTIFACT="${PROJECT_DIR}/target/vagrant-${BUILD}-${BUILD_NUMBER}/${BUILD}.qcow2"
-VAGRANT_BOX_ARTIFACT="${PROJECT_DIR}/target/${BUILD}-${BUILD_NUMBER}.box"
-
-if [ -n "${BUILD_NAME_PREFIX}" ]; then
-    BASE_QEMU_ARTIFACT=$(cd "${PROJECT_DIR}/bin"; bash ./find-build-artifact.sh --number "${BUILD_NUMBER}" --prefix "${BUILD_NAME_PREFIX}"  --type "${BUILD_TYPE}")
-    echo "${BASE_QEMU_ARTIFACT}"
-    echo "not implemented"
-    exit 1
-fi
-
 BUILD_SRC_DIR=$(find "${PROJECT_DIR}/src" -type d -name "${BUILD}" | head -n 2)
-assert_qemu_build_src "${BUILD_SRC_DIR}"
 
-echo "building base artifact: ${BASE_QEMU_ARTIFACT}"
-(cd "${BUILD_SRC_DIR}"; packer build --var "build_id=${BUILD_NUMBER}" ./manifest.qemu.packer.json)
+if [ -z "${BASE_QEMU_ARTIFACT}" ]; then
+    BASE_QEMU_ARTIFACT="${PROJECT_DIR}/target/${BUILD}-${BUILD_NUMBER}/${BUILD}.qcow2"
+    (cd "${BUILD_SRC_DIR}"; packer build --var "build_id=${BUILD_NUMBER}" ./manifest.qemu.packer.json)
+fi
+
 if [ ! -f "${BASE_QEMU_ARTIFACT}" ]; then
-    echo "failed to build base qemu artifact ${BASE_QEMU_ARTIFACT}"
+    echo "failed to build base qemu artifact ${BASE_QEMU_ARTIFACT}" 1>&2
     exit 1
 fi
 
-echo "building vagrant artifact: ${VAGRANT_QEMU_ARTIFACT}"
-(cd "${BUILD_SRC_DIR}"; packer build --var "build_id=${BUILD_NUMBER}" --var "iso_url=${BASE_QEMU_ARTIFACT}"  ./vagrant.manifest.qemu.packer.json)
+VAGRANT_QEMU_ARTIFACT="${PROJECT_DIR}/target/vagrant-${BUILD}-${BUILD_NUMBER}/${BUILD}.qcow2"
+echo "building vagrant qemu artifact: ${VAGRANT_QEMU_ARTIFACT}"
+(cd "${BUILD_SRC_DIR}"; packer build --var "build_name=${BUILD}" --var "build_id=${BUILD_NUMBER}" --var "iso_url=${BASE_QEMU_ARTIFACT}"  ./vagrant.manifest.qemu.packer.json)
 if [ ! -f "${VAGRANT_QEMU_ARTIFACT}" ]; then
-    echo "failed to build vagrant qemu artifact ${VAGRANT_QEMU_ARTIFACT}"
+    echo "failed to build vagrant qemu artifact ${VAGRANT_QEMU_ARTIFACT}" 1>&2
     exit 1
 fi
 
+VAGRANT_BOX_ARTIFACT="${PROJECT_DIR}/target/${BUILD}-${BUILD_NUMBER}.box"
 echo "converting ${VAGRANT_QEMU_ARTIFACT} to a vagrant box ${VAGRANT_BOX_ARTIFACT}"
 (cd "${PROJECT_DIR}/bin"; bash ./qemu2vagrant.build-box.sh  "${VAGRANT_QEMU_ARTIFACT}" "${VAGRANT_BOX_ARTIFACT}")
 if [ ! -f "${VAGRANT_BOX_ARTIFACT}" ]; then
-    echo "failed to build vagrant box ${VAGRANT_BOX_ARTIFACT}"
+    echo "failed to build vagrant box ${VAGRANT_BOX_ARTIFACT}" 1>&2
     exit 1
 fi
 
