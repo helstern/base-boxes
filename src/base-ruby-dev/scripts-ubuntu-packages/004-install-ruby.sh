89e862d1090f193dd9186d19cbaa3a918e2151ad
radu helstern <radu.helstern@gmail.com>
2015-06-14 23:31:08 +0100

    stop install rvm

--- src/base-ruby-dev/scripts-ubuntu-packages/004-install-ruby.sh
+++ src/base-ruby-dev/scripts-ubuntu-packages/004-install-ruby.sh
@@ -13,12 +13,11 @@ if [ "${CURRENT_USER}" != "${RUBY_USER}" ]; then
     exit 1
 fi
 
-curl -sSL https://rvm.io/mpapis.asc | gpg --import -
-
-echo "install rvm in single user (${RUBY_USER}) mode"
-echo "see https://rvm.io/rvm/install"
-
-curl -sSL https://get.rvm.io | bash -s stable --ruby
+# install rvm
+#curl -sSL https://rvm.io/mpapis.asc | gpg --import -
+#echo "install rvm in single user (${RUBY_USER}) mode"
+#echo "see https://rvm.io/rvm/install"
+#curl -sSL https://get.rvm.io | bash -s stable --ruby
 
 RUBY_INSTALL_VERSION=0.5.0
 echo "install ruby-install ${RUBY_INSTALL_VERSION}"
@@ -38,19 +37,36 @@ sudo make install
 
 RUBY_VERSIONS="2.1.3 2.2.1"
 for ruby_version in ${RUBY_VERSIONS}; do
-    ruby-install --rubies-dir ~/.rvm/rubies ruby "${ruby_version}"
+#    ruby-install --rubies-dir ~/.rvm/rubies ruby "${ruby_version}"
+    ruby-install ruby "${ruby_version}"
 done
+
+cat <<BASH_PROFILE >> ~/.bash_profile
+
+# auto-added via packer
+source /usr/local/share/chruby/chruby.sh
+source /usr/local/share/chruby/auto.sh
+BASH_PROFILE
+
+cat <<BASHRC >> ~/.bashrc
+
+# auto-added via packer
+source /usr/local/share/chruby/chruby.sh
+source /usr/local/share/chruby/auto.sh
+BASHRC
+
 
 source /usr/local/share/chruby/chruby.sh
 source /usr/local/share/chruby/auto.sh
-# required for chruby
-RUBIES+=(~/.rvm/rubies/*)
+# required for rmv - chruby integration
+# RUBIES+=(~/.rvm/rubies/*)
 
-RUBY_GEMS="bundler passenger rails"
+RUBY_GEMS="bundler passenger rails rspec pry pry-debugger cucumber less sass ruby-debug-ide"
 for ruby_version in ${RUBY_VERSIONS}; do
     chruby ruby "${ruby_version}"
+    gem update --system
     for package in ${RUBY_GEMS}; do
-        gem install "${package}"
+        gem install --no-ri --no-rdoc "${package}"
     done
 done
 
